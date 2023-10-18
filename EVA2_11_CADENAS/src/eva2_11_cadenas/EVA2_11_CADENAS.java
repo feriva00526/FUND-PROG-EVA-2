83e7aa06bc2fb06f432f7e111f4f79d587d34442
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-10-18 16:53:36 -0700

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_11_cadenas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_11_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Scanner input = new Scanner(System.in); 
+          
+          String cade = "hola";
+          if (cade.equals( "hola"))
+          System.out.println("las cadenas son iguales");
+          else
+          System.out.println("las cadenas no son iguales");
+          
+    }
+    
+}
