fd89908058f72439db7a07dd4627d6dbf821f41d
feriva00526 <143562691+feriva00526@users.noreply.github.com>
2023-10-30 17:46:52 -0600

    Add files via upload

---
+++ EVA2_17_WHILE/src/eva2_17_while/EVA2_17_WHILE.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_17_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_17_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+          // TODO code application logic here
+          int i = 0;
+        Scanner input = new Scanner(System.in);
+     while( i < 10){
+     
+         System.out.println(i + " - " + "Ivan");
+         
+         i++;
+     }
+
+        // TODO code application logic here
+    }
+    
+}
